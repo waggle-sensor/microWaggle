44715f7ac224cfc35a56180790198b2d3d491133
lakithaomal <lakithaomalanl@gmail.com>
2018-09-28 05:41:44 -0500

    MicroWaggle firmware edit

--- integrated/firmware/microWaggle.ino
+++ integrated/firmware/microWaggle.ino
@@ -10,7 +10,7 @@ class Sensor
         int sensorId;
         String sensorName;
         int sensorType;
-        
+
     public:
         Sensor(int id, String name){
             sensorId = id;
@@ -19,52 +19,52 @@ class Sensor
         int getSensorId(){
             return sensorId;
         }
-        
+
         String getSensorName(){
             return sensorName;
         }
-        
+
         void setSensorId(int id)
         {
             sensorId = id;
         }
-        
+
         int getSensingFrequency(){
             return sensingFrequency;
         }
-        
+
         void setSensingFrequency(int newSensFreq){
             sensingFrequency = newSensFreq;
         }
-        
+
         void setType(int type){
             sensorType= type;
         }
-        
-        
+
+
         bool getEnabled(){
             return enabled;
         }
-        
+
         void setEnabled(bool newEnabled){
             enabled = newEnabled;
         }
-        
+
         int getLastSensingTime()
         {
             return lastSensingTime;
         }
-        
+
         int getType(){
-            return sensorType; 
+            return sensorType;
         }
-        
-        
+
+
         void setLastSensingTime(int t)
         {
             lastSensingTime = t;
         }
-        
+
 };
 Sensor* tempSensor;
 Sensor* humiditySensor;
@@ -82,13 +82,13 @@ String allSensorgrams; //stores all the sensorgrams appended on to each other. t
 
 int lastSendTime  = Time.now();
 int lastStatusTime = Time.now();
-int statusFreq = 60; 
+int statusFreq = 60;
 int maxPublishingLength = 255; // indicates the max amt of characters that you can send in a Particle.publish() call (it is actually around 620, but it's 100 for now for testing)
 
 void setup() {
     sensorIdMap.insert(std::make_pair(1, new Sensor(1, "tempSensor")));
     sensorIdMap.insert(std::make_pair(2, new Sensor(2, "humiditySensor")));
-    
+
     sensorsPtrs[0] = sensorIdMap[1];
     sensorsPtrs[1] = sensorIdMap[2];
     Particle.function("sensorConfig", sensorConfig);
@@ -103,13 +103,13 @@ void loop() {
     uint16_t data = 5;
 
     // check if time to send status msg
-    if (Time.now() - lastStatusTime >= statusFreq) 
+    if (Time.now() - lastStatusTime >= statusFreq)
     {
         status = getStatusMsg();
         Particle.publish("microWaggleStatus", status, PRIVATE);
         lastStatusTime = Time.now();
-    }    
-    
+    }
+
     // check if time to report the sensorgrams (send sensorgrams)
     if(Time.now() - lastSendTime >= reportingFreq) //time to send/create sensorgram
     {
@@ -117,7 +117,7 @@ void loop() {
         if(Particle.connected() && allSensorgrams.length() != 0) // particle is connected and atleast one sensorgram is there
             PublishData();
     }
-    
+
     // check if time to measure with sensor
     for(Sensor *s : sensorsPtrs)
     {
@@ -127,37 +127,9 @@ void loop() {
             String sensorgram = pack((unsigned short)(s->getSensorId()), sensor_instance, param_id, data, s->getLastSensingTime()); // just some dummy data
             allSensorgrams += sensorgram;
             if(allSensorgrams.length() >= maxPublishingLength-15) // if too much of sensorgrams build up, we put a semicolon to indicate that for the next publish, we only send till the semicolon (so we don't send too much in one send)
-                allSensorgrams += ";"; 
+                allSensorgrams += ";";
         }
     }
-}
-
-int sensorConfig(String param)
-{
-    int index1 = param.indexOf(";");
-    int index2 = param.lastIndexOf(";");
-
-    //TODO: Check for invalid input (i.e., message is not in right format like this "id;status;freq")
-    //checking if only 1 semi colon is in param or if there is no semicolon
-    if(index1 == index2 || index1 == -1 || index2 == -1) //how do i throw an error message?
-        Particle.publish("ERROR", "sensorConfig(): invalid input - data format must be 'id;status;freq'");
-    
-    int sensorId = param.substring(0,index1).toInt();
-    String sensorStatus = param.substring(index1+1, index2);
-    String freq = param.substring(index2+1); // if we don't want to fix frequency, put an _
-    
-    bool isSensorIdValid = check_key(sensorId); //checks if inputted sensorId is an existing sensorId
-    if(!isSensorIdValid)
-        Particle.publish("ERROR", "sensorConfig(): inputted sensorId does not exist");
-
-    if(sensorStatus == "en")
-        sensorIdMap[sensorId]->setEnabled(true);
-    else if(sensorStatus == "dis")
-        sensorIdMap[sensorId]->setEnabled(false);
-    if(freq != "_" && freq.toInt() >= thresholdSensorFreq) // if freq is not a num, then freq.toInt() returns 0
-        sensorIdMap[sensorId]->setSensingFrequency(freq.toInt());
-    
-    return 0;
 }
 
 
@@ -172,12 +144,12 @@ String getStatusMsg()
     }
     status += "SD:" + boolToEnabledDisabled(sdCardEnabled) + ";";
     status += "ReportFreq:" + String(reportingFreq);
-
+    status += "StatusFreq:" + String(statusFreq);
     return status;
 }
 
 //returns "en" if true, and "dis" if false
-String boolToEnabledDisabled(bool b) 
+String boolToEnabledDisabled(bool b)
 {
     if(b)
         return "en";
@@ -194,6 +166,36 @@ bool check_key(int key)
 
     return true;
 }
+
+int sensorConfig(String param)
+{
+    int index1 = param.indexOf(";");
+    int index2 = param.lastIndexOf(";");
+
+    //TODO: Check for invalid input (i.e., message is not in right format like this "id;status;freq")
+    //checking if only 1 semi colon is in param or if there is no semicolon
+    if(index1 == index2 || index1 == -1 || index2 == -1) //how do i throw an error message?
+        Particle.publish("ERROR", "sensorConfig(): invalid input - data format must be 'id;status;freq'");
+
+    int sensorId = param.substring(0,index1).toInt();
+    String sensorStatus = param.substring(index1+1, index2);
+    String freq = param.substring(index2+1); // if we don't want to fix frequency, put an _
+
+    bool isSensorIdValid = check_key(sensorId); //checks if inputted sensorId is an existing sensorId
+    if(!isSensorIdValid)
+        Particle.publish("ERROR", "sensorConfig(): inputted sensorId does not exist");
+
+    if(sensorStatus == "en")
+        sensorIdMap[sensorId]->setEnabled(true);
+    else if(sensorStatus == "dis")
+        sensorIdMap[sensorId]->setEnabled(false);
+    if(freq != "_" && freq.toInt() >= thresholdSensorFreq) // if freq is not a num, then freq.toInt() returns 0
+        sensorIdMap[sensorId]->setSensingFrequency(freq.toInt());
+
+    return 0;
+}
+
+
 
 int nodeConfig(String param) {
     param = param.toLowerCase();
@@ -213,12 +215,24 @@ int nodeConfig(String param) {
     else if (param == "disablesd") {
         sdCardEnabled = false;
     }
+
+    else if (param.indexOf("statusfreq-") != -1) {
+
+        int statusFrequency = param.substring(11).toInt(); // * if inputted freq of reporting is not a num, inputReportFreq = 0 (.toInt() returns 0)
+
+        if (statusFrequency<5)  // cannot change freq
+            Particle.publish("ERROR", "nodeConfig(): requested status frequency of " + param.substring(11) + " seconds is too low. Must be greater than 5 seconds.");
+        else
+             statusFreq = statusFrequency;
+    }
+
+
     else if (param.indexOf("freqreport-") != -1) {
-        
+
         int inputReportFreq = param.substring(11).toInt(); // * if inputted freq of reporting is not a num, inputReportFreq = 0 (.toInt() returns 0)
         int minSensing = inputReportFreq; //minimum possible reporting frequency allowed --> based on the max sensor sensing freq
         bool canChangeFreq = true; // can the reporting frequency be changed?
-        
+
         for (Sensor *s : sensorsPtrs) {
             if (s->getSensingFrequency() > minSensing) // a sensor sensingFreq is > the requested reporting frequency --> this is bad...
             {
@@ -231,6 +245,8 @@ int nodeConfig(String param) {
         else
             reportingFreq = inputReportFreq;
     }
+
+
     else
         Particle.publish("ERROR", "nodeConfig: command not found");
     return 0;
@@ -244,16 +260,17 @@ int nodeConfig(String param) {
 void PublishData()
 {
 
-    int delimiterIndex = allSensorgrams.indexOf(";");
-    String sendData; 
-    if(delimiterIndex != -1){ // if delimeter exists, then only send up till the delimeter (b/c delimeter indicates the max amount you can send in one publish)
-        sendData = allSensorgrams.substring(0, delimiterIndex); //TODO: Check if this works correctly...
-        allSensorgrams = allSensorgrams.substring(delimiterIndex+1); // set allSensorgrams to everything after the first semi colon
+        int delimiterIndex = allSensorgrams.indexOf(";");
+        String sendData;
+        if(delimiterIndex != -1){ // if delimeter exists, then only send up till the delimeter (b/c delimeter indicates the max amount you can send in one publish)
+            sendData = allSensorgrams.substring(0, delimiterIndex); //TODO: Check if this works correctly...
+            allSensorgrams = allSensorgrams.substring(delimiterIndex+1); // set allSensorgrams to everything after the first semi colon
+        }
+        else // no delimeter, so just send all that we have right now
+            sendData = allSensorgrams;
+   if(sendData.length()>1){
+        Particle.publish("sensorgram", sendData, PRIVATE);
     }
-    else // no delimeter, so just send all that we have right now
-        sendData = allSensorgrams;
-
-    Particle.publish("sensorgram", sendData, PRIVATE);
 }
 
 String pack(unsigned short sensorID, unsigned char sensor_instance, unsigned char parameter_id,  uint16_t data, unsigned int cTime) {
@@ -263,8 +280,8 @@ String pack(unsigned short sensorID, unsigned char sensor_instance, unsigned cha
 	String parameterIdHex = CharToHex(parameter_id);
 	String dataHex = Int_16_to_Hex(&data);
 	String cTimeHex = IntToHex(cTime);
-    String type = "15";
-    String lengthOfData = "0002";
+  String type = "15";
+  String lengthOfData = "0002";
 	String sensorgramString = lengthOfData + sensorIDHex + sensorInstanceHex + parameterIdHex +  cTimeHex + type +dataHex+";";
 
 	// [Length (2B)] [Sensor_ID (2B)] [Sensor_Instance (1B)] [Parameter_ID (1B)] [ Sampling_Time (4B)] [ Data_Format (1B)] [data (xB)]
@@ -335,11 +352,9 @@ String Char_Byte_2_Hex(char val){
         }
         else{
         Final =  Hex;
-    
+
         }
-    
+
     return Final;
-    
+
 }
-
-
